c6876fb8928268f93cb9103e0ac724438c74451d
arya sinha <arya20498@iiitd.ac.in>
2021-11-27 15:40:48 +0530

    update

--- Assignment4/src/Image.java
+++ Assignment4/src/Image.java
@@ -1,20 +1,46 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Image <T>{
 	private ArrayList<ArrayList<T>> matrix = new ArrayList<>();
+	private int rows;
+	private int cols;
+	
+	static {
+		Scanner sc =new Scanner(System.in);
+	}
+	
+	{
+		System.out.print("Enter number of rows: ");
+		this.rows = sc.nextInt();
+		System.out.print("Enter number of cols: ");
+		this.cols = sc.nextInt();
+	}
 	
 	Image() {
-		
+		for (int i=0; i<rows; i++)
+			for (int j=0; j<cols; j++) ;
+				
+	}
+	
+	public T getpixel() {
+		return new T();
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Enter 0 to enter image details and 1 to randomize it");
+		System.out.println("Enter 0 to enter image details and 1 to randomize it");
 		Image<colorImagePixel> colorpic = new Image<>();
 
 	}
 	
 	public static <I> void getNegative(Image I) {
 		
+	}
+	
+	public static void displayMenu() {
+		System.out.println("1. Create image/n2. Input image/n3. Update image/n4. Compute negative of image/n5. Display image/n");
 	}
 
 }
